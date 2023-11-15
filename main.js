43a071828297645b19315707fc04b188c0a9191a
github.com-chev0100 <chev0100@algonquinlive.com>
2023-11-15 15:28:38 -0500

    built functions to add beers to orders and display in orders list

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // create an array called beerMenu to store the beer objects
 const beerMenu = [];
-console.log(beerMenu);
+const orders = [];
 
 // create a function called fetchBeers() to fetch a list of beers from https://random-data-api.com/api/v2/beers
 function init() {
@@ -8,7 +8,7 @@ function init() {
 	fetchBeers();
 
 	// use if you want to create a button that fetches the beers instead of loading them on initialization
-	// document.querySelector('#beerList').addEventListener('click', () => {});
+	document.querySelector('#beerList').addEventListener('click', orderBeer);
 }
 
 // run the fetchBeers() function when the app is initialized
@@ -49,17 +49,17 @@ function fetchBeers() {
 // create a function called displayBeers() to display the list of beers in the beerList <ul>
 
 // build the list of beers with the following markup:
-        // `
-            //   <li class="beer" data-id="${beer.id}">
-            //     <div class="beer__info">
-            //       <p class="beer__name">${beer.name}</p>
-            //       <p class="beer__style">${beer.style}</p>
-            //       <p class="beer__abv">${beer.alcohol}</p>
-            //     </div>
-            //     <p class="beer__price">$${beer.price}</p>
-            //     <button id="addBeerButton" class="beer__button">Add to Order</button>
-            //   </li>
-          // `
+// `
+//   <li class="beer" data-id="${beer.id}">
+//     <div class="beer__info">
+//       <p class="beer__name">${beer.name}</p>
+//       <p class="beer__style">${beer.style}</p>
+//       <p class="beer__abv">${beer.alcohol}</p>
+//     </div>
+//     <p class="beer__price">$${beer.price}</p>
+//     <button id="addBeerButton" class="beer__button">Add to Order</button>
+//   </li>
+// `
 
 function displayBeers(beers) {
 	console.log(beers);
@@ -80,7 +80,7 @@ function displayBeers(beers) {
             `;
 		})
 		.join('');
-	console.log(html);
+	// console.log(html);
 	beerList.innerHTML = html;
 }
 
@@ -100,28 +100,56 @@ WHERE WE WILL START DURING SESSION TWO
 
 // create a function called orderBeer() to add a beer to an order array when the user clicks the "Add to Order" button
 
-// use ev.target, .matched(), and .closest() to get the beer id from the clicked beer
+function orderBeer(ev) {
+	// use ev.target, .matched(), and .closest() to get the beer id from the clicked beer
+	if (ev.target.matches('#addBeerButton')) {
+		console.log(ev.target.closest('.beer'));
+		const listId = +ev.target.closest('.beer').dataset.id;
+		console.log(listId);
 
-// create an object called order and include the id, name, and price. create an orders array to store the orders. push each order to an array of orders
+		const beer = beerMenu.find((beer) => beer.id === listId);
+
+		orders.push(beer);
+
+		displayOrders(orders);
+
+		console.log(beer);
+	} else {
+		console.log('wrong place');
+	}
+}
+
+// create a function called displayOrders() to display the orders from the orders array in the orderList <ul>
+
+function displayOrders(orderArr) {
+	const orderList = document.querySelector('#orderList');
+	console.log(orderArr);
+
+	let html = orderArr
+		.map((order) => {
+			return `
+	      <li class="order" data-id="${order.id}">
+	        <p class="order__name">${order.name}</p>
+	        <p class="order__price">$${order.price}</p>
+	      </li>
+	    `;
+		})
+		.join('');
+
+	console.log(html);
+	orderList.innerHTML = html;
+}
+
+// create an object called order and include the id, name, price, and tally. create an orders array to store the orders. push each order to an array of orders
 
 // const order = {
 // 	id: beer.id,
 // 	name: beer.name,
 // 	price: beer.price,
+// 	tally: 1,
 // };
 
-// create a function called displayOrders() to display the orders from the orders array in the orderList <ul>
-
-// build the list of orders with the following markup:
-// `
-// 	  <li class="order" data-id="${order.id}">
-// 	    <p class="order__name">${order.beer}</p>
-// 	    <p class="order__price">$${price}</p>
-// 	  </li>
-// 	`
-
-// modify the orderBeer() function to check if the beer is already in the orders array. if it is, increment the quantity by 1. if it is not, add the beer to the orders array
+// modify the orderBeer() function to check if the beer is already in the orders array. if it is, increment the tally by 1. if it is not, add the beer to the orders array
 
 // initialize the app
-
 document.addEventListener('DOMContentLoaded', init);
